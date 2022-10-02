dfd85a25dc8fe116d5cb8df68d6b3e3f8dcf2a34
Sovit2000 <113337996+Sovit2000@users.noreply.github.com>
2022-10-02 13:46:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 const http = require("http");
 const fs = require("fs");
-const args = require('minimist')(process.args.slice(2));
-
-let homecontent = "";
-let projectcontent = "";
-let registrationContent = "";
-
-fs.readFile("home.html", (err, home) => {
-    console.log(home.toString());
-  });
-
-  fs.readFile("home.html", (err, home) => {
-    if (err) {
-      throw err;
-    }
-    http
-      .createServer((request, response) => {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(home);
-        response.end();
-      })
-      .listen(args.port = 5000);
-  });
-
-  const http = require("http");
-const fs = require("fs");
 
 let homeContent = "";
 let projectContent = "";
+let registrationContent="";
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
@@ -43,19 +19,32 @@ fs.readFile("project.html", (err, project) => {
   projectContent = project;
 });
 
-http
-  .createServer((request, response) => {
+fs.readFile("registration.html",(err,reg) => {
+  if (err){
+    throw err;
+  }
+  registrationContent=reg;
+});
+
+let args=require("minimist")(process.argv.slice(2));
+
+http.createServer((request, response) => {
     let url = request.url;
     response.writeHeader(200, { "Content-Type": "text/html" });
     switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
+        case "/project":
+          response.write(projectContent);
+          response.end();
+          break;
+        case "/registration":
+          response.write(registrationContent);
+          response.end();
+          break;
+
+        default:
+          response.write(homeContent);
+          response.end();
+          break;
     }
   })
-  .listen(args.port = 5000);
+  .listen(args["port"]);
